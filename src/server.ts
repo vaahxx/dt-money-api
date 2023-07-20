8ed955f520568ea098476b92110b331d3ca71499
Valentina Garavaglia <vaahxx@gmail.com>
2023-07-20 14:07:02 -0400

    fix: added host: '0.0.0.0' to app.listen()

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('🚀 Server ready at http://localhost:3333')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 Server ready at http://localhost:3333')
+  })
