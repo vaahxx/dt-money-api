c2d4ad1d6a307cd95d9263d79fef1a7adf80ad11
Valentina Garavaglia <vaahxx@gmail.com>
2023-07-22 20:40:00 -0400

    chore: test adding request hostname

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -83,6 +83,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
         sessionId = crypto.randomUUID()
         reply.cookie('sessionId', sessionId, {
           path: '/',
+          domain: request.hostname,
           maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
         })
       }
