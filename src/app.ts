8af837028d4ae600ba3e3271fac8ed8a23ffa043
Valentina Garavaglia <vaahxx@gmail.com>
2023-07-22 20:17:56 -0400

    chore: remove fastify cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
-import fastifyCors from 'fastify-cors'
+import cors from '@fastify/cors'
 import { transactionsRoutes } from './routes/transactions'
 
 export const app = fastify()
 
 app.register(cookie)
 
-app.register(fastifyCors, {
+app.register(cors, {
   origin: true,
   credentials: true,
 })
