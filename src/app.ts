3fce0fb82ab1b1a7b724ec34dc67724244f0a06e
Valentina Garavaglia <vaahxx@gmail.com>
2023-07-22 20:21:53 -0400

    chore: cors

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,9 @@ export const app = fastify()
 
 app.register(cookie)
 
-app.addHook('onSend', (request, reply, payload, done) => {
-  reply.header('Access-Control-Allow-Origin', '*')
-  reply.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  )
-  reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  done(null, payload)
+app.register(cors, {
+  origin: true,
+  credentials: true,
 })
 
 app.register(transactionsRoutes, {
